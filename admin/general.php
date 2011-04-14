3a8d48c9a5e5cc1ba6925cd5252baf1d013f66a9
Spencer <spencerfinnell@gmail.com>
2011-04-14 11:40:05 -0400

    Remove Posts from the admin menu.

--- admin/general.php
+++ admin/general.php
@@ -2,51 +2,66 @@
 /**
  *
  *
- * @since PITS 0.1
+ * @since Alfred 0.1
  */
  
 /**
  * Add a separator to the WordPress admin menus
  *
- * @since PITS 0.1
+ * @since Alfred 0.1
  */
-function pits_admin_separator () {
+function alfred_admin_separator () {
 	global $menu;
 
-	$menu[] = array( '', 'read', 'separator-pits', '', 'wp-menu-separator' );
+	$menu[] = array( '', 'read', 'separator-alfred', '', 'wp-menu-separator' );
 }
-add_action( 'admin_menu', 'pits_admin_separator' );
+add_action( 'admin_menu', 'alfred_admin_separator' );
 
 /**
  * Move our custom separator above our custom post types
  *
- * @since PITS 0.1
+ * @since Alfred 0.1
  *
  * @param array $menu_order
  */
-function pits_admin_menu_order( $menu_order ) {
+function alfred_admin_menu_order( $menu_order ) {
 	// Initialize our custom order array
-	$pits_menu_order = array();
+	$alfred_menu_order = array();
 
 	// Get the index of our custom separator
-	$pits_separator = array_search( 'separator-pits', $menu_order );
+	$alfred_separator = array_search( 'separator-alfred', $menu_order );
 
 	// Loop through menu order and do some rearranging
 	foreach ( $menu_order as $index => $item ) {
 
 		// Current item is our forum CPT, so set our separator here
 		if ( ( 'upload.php' == $item ) ) {
-			$pits_menu_order[] = 'separator-pits';
-			unset( $menu_order[$pits_separator] );
+			$alfred_menu_order[] = 'separator-alfred';
+			unset( $menu_order[$alfred_separator] );
 		}
 
 		// Skip our separator
-		if ( 'separator-pits' != $item )
-			$pits_menu_order[] = $item;
+		if ( 'separator-alfred' != $item )
+			$alfred_menu_order[] = $item;
 	}
 
 	// Return our custom order
-	return $pits_menu_order;
+	return $alfred_menu_order;
 }
-add_action( 'menu_order', 'pits_admin_menu_order' );
+add_action( 'menu_order', 'alfred_admin_menu_order' );
 add_action( 'custom_menu_order', '__return_true' );
+
+/**
+ * Unset "Posts" from the menu.
+ *
+ * @since Alfred 0.1
+ */
+function alfred_unset_post() {
+	global $menu;
+
+	if( isset( $menu[5] ) )
+		unset( $menu[5] );
+		
+	return $menu;
+}
+add_action( 'admin_menu', 'alfred_unset_post' );
