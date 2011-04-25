1efc9074d80f44bae475679b821bf8f85106195c
Spencer <spencerfinnell@gmail.com>
2011-04-25 12:55:53 -0400

    Remove functions for a simplier approach (in client-functions.php)

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -6,34 +6,3 @@ function alfred_rewrite_authors() {
 	$wp_rewrite->author_base = "user";
 }
 add_action( 'alfred_generate_rewrite_rules', 'alfred_rewrite_authors' );
-
-function alfred_client_info( $post_id = '' ) {
-	global $alfred, $post;
-	
-	if ( ! isset ( $post_id ) )
-		$post = get_post( $post );
-		
-	if ( ! $post )
-		return false;
-	
-	if ( $post->post_type != $alfred->client_post_type )
-		return false;
-		
-	$client_info = get_post_meta( $post->ID, 'client', true );
-	
-	if ( $client_info )
-		return $client_info;
-		
-	return false;
-}
-
-function alfred_client_contact_info( $post_id = '' ) {
-	global $post;
-	
-	if ( ! isset ( $post_id ) )
-		$post = get_post( $post );
-	
-	$contact_info = alfred_client_info( $post_id );
-	
-	return $contact_info[ 'contact' ];
-}
