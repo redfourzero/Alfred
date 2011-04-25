aa3997f91c5c924ea551c4b479b35d311091f065
Spencer <spencerfinnell@gmail.com>
2011-04-25 13:02:19 -0400

    Allow the new author slug to be filtered.

--- includes/user-functions.php
+++ includes/user-functions.php
@@ -1,8 +1,22 @@
 <?php
+/**
+ * User-related functions.
+ *
+ * These are separate from Clients. However, some extra
+ * tweaking is needed to make the default WordPress user system
+ * work to our liking.
+ *
+ * @since Alfred 0.1
+ */
 
+/**
+ * Change the author slug in the URI to "user" instead of "author"
+ *
+ * @since Alfred 0.1
+ */
 function alfred_rewrite_authors() {
 	global $wp_rewrite;
 	
-	$wp_rewrite->author_base = "user";
+	$wp_rewrite->author_base = apply_filters( 'alfred_author_slug', 'user' );
 }
 add_action( 'alfred_generate_rewrite_rules', 'alfred_rewrite_authors' );
