c988adb24bd37b42076e874e4eefde4742f510cf
Spencer <spencerfinnell@gmail.com>
2011-05-17 19:50:16 -0400

    First pass at saving time.

--- js/jquery.alfred.js
+++ js/jquery.alfred.js
@@ -1,6 +1,15 @@
 jQuery(document).ready(function($) {
 	$( '#stopwatch-clock' ).stopwatch();
 	
+	var stopwatch_end = $( '#_log_duration' ).val();
+	if ( stopwatch_end != "" ) {
+		stopwatch_end = $( '#_log_duration' ).val().split( ':' );
+		
+		$( '#stopwatch-clock .display .sec' ).html( stopwatch_end[2] );
+		$( '#stopwatch-clock .display .min' ).html( stopwatch_end[1] );
+		$( '#stopwatch-clock .display .hr' ).html( stopwatch_end[0] );
+	}
+	
 	$( '#stopwatch-clock .start' ).click(function() {
 		var data = {
 			action	 : 'alfred_stopwatch_update',
