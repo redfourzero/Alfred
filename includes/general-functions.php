bdf02e33e409d4e79db55c56446c52f46556dd48
Spencer <spencerfinnell@gmail.com>
2011-05-17 14:18:47 -0400

    First pass at basic jQuery enqueueing.

--- includes/general-functions.php
+++ includes/general-functions.php
@@ -4,3 +4,10 @@
  *
  * @since Alfred 0.1
  */
+ 
+function alfred_enqueue_scripts() {
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jquery-stopwatch', get_stylesheet_directory_uri() . '/js/jquery.stopwatch.js' );
+	wp_enqueue_script( 'jquery-alfred', get_stylesheet_directory_uri() . '/js/jquery.alfred.js' );
+}
+add_action( 'alfred_init', 'alfred_enqueue_scripts' );
