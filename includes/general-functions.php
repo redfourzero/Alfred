b9f9f211ffdfa676cf3eee74ac32fe07be1eabc5
Spencer <spencerfinnell@gmail.com>
2011-05-17 15:02:19 -0400

    Introduce admin-only CSS.

--- includes/general-functions.php
+++ includes/general-functions.php
@@ -5,9 +5,25 @@
  * @since Alfred 0.1
  */
  
+/**
+ * Enqueue Extra Scripts
+ *
+ * @since Alfred 0.1
+ */
 function alfred_enqueue_scripts() {
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'jquery-stopwatch', get_stylesheet_directory_uri() . '/js/jquery.stopwatch.js' );
 	wp_enqueue_script( 'jquery-alfred', get_stylesheet_directory_uri() . '/js/jquery.alfred.js' );
 }
-add_action( 'alfred_init', 'alfred_enqueue_scripts' );
+add_action( 'wp_print_scripts', 'alfred_enqueue_scripts' );
+
+/**
+ * Enqueue Extra Styles
+ *
+ * @since Alfred 0.1
+ */
+function alfred_enqueue_styles() {
+	if ( is_admin() )
+		wp_enqueue_style( 'alfred-admin', get_stylesheet_directory_uri() . '/admin/css/admin.css' );
+}
+add_action( 'admin_print_styles', 'alfred_enqueue_styles' );
--- js/jquery.alfred.js
+++ js/jquery.alfred.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function($) {
-
+	$( '#stopwatch-clock' ).stopwatch();
+	
+	$( '#stopwatch-clock .start' ).click(function() {
+		alert( 'Stopwatch has been started.' );
+	});
+	
+	$( '#stopwatch-clock .stop' ).click(function() {
+		alert( 'Stopwatch has been stopped.' );
+	});
+	
+	$( '#stopwatch-clock .reset' ).click(function() {
+		alert( 'Stopwatch has been reset.' );
+	});
 });
